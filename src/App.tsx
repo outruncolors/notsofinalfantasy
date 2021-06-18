e4156ea901139958a85d8c1ad71def743cef46ae
Outrun Colors, LLC <outruncolors@gmail.com>
2021-06-17 22:09:44 -0500

    Add background image.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Card, Layout } from "antd";
+import { Alert, Card, Menu, Layout } from "antd";
 import { Page } from "./Page";
 import "./App.less";
 import logo from "./logo.png";
@@ -6,14 +6,43 @@ import logo from "./logo.png";
 function App() {
   return (
     <Layout>
+      <Layout.Header style={{ margin: "2rem 11rem", alignItems: "flex-end" }}>
+        <Card
+          style={{
+            margin: "4rem",
+            maxWidth: 500,
+            borderRadius: 4,
+            border: "1px solid white",
+          }}
+          cover={<img alt="Logo" src={logo} style={{}} />}
+          hoverable={true}
+          bordered={false}
+          bodyStyle={{ display: "none" }}
+        />
+      </Layout.Header>
       <Layout.Content>
         <Page>
-          <Card
-            style={{ margin: "4rem", maxWidth: 500 }}
-            cover={<img alt="Logo" src={logo} style={{}} />}
-            hoverable={true}
-            bordered={false}
-            bodyStyle={{ display: "none" }}
+          <Menu mode="horizontal" theme="light">
+            <Menu.Item key="home">About</Menu.Item>
+            <Menu.Item key="how-to-play">How to Play</Menu.Item>
+            <Menu.Item key="jobs">Job System</Menu.Item>
+            <Menu.Item key="enemy-glossary">Enemy Glossary</Menu.Item>
+            <Menu.Item key="contribute">Contribute</Menu.Item>
+          </Menu>
+          <Alert
+            type="info"
+            message="Paying Tokens to Contributors"
+            description={
+              <>
+                I am currently looking for talented Rec Room artists to make
+                everything prettier! I'm offering to provide tokens (and credit,
+                of course!) based on the work done. Visit the Contribute page
+                for more info!
+              </>
+            }
+            style={{ margin: "2rem" }}
+            showIcon={true}
+            closable={true}
           />
         </Page>
       </Layout.Content>
